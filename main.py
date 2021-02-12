d384b5b9564fa2819b4ccaa334edac1943799dca
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-02-12 18:09:45 +0100

    Using function to choose dynamically steps's numbers and tall

--- main.py
+++ main.py
@@ -3,4 +3,30 @@ import turtle
 
 t = turtle.Turtle()
 
+
+# creat function avoid repetition
+def leftfor(tall):
+    t.forward(tall)
+    t.left(90)
+
+
+def rightfor(tall):
+    t.forward(tall)
+    t.right(90)
+
+
+# function to choose tall + number of steps
+def steps(tall, number_of_steps):
+    for i in range(0, number_of_steps):
+        leftfor(tall)
+        rightfor(tall)
+        # Lower steps
+        tall -= 5
+    t.forward(tall)
+
+
+# calling steps function dynamically
+steps(40, 6)
+
+
 turtle.done()
